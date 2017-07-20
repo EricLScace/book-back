2146511a3bb38aceac262bd77354f5907f177542
EricLScace <mail@ericscace.com>
2017-07-19 22:11:05 -0400

    Review db:migrations
    what:
    * made name required in authors
    * made title required in books
    
    Issue#13

--- db/migrate/20170720015643_create_authors.rb
+++ db/migrate/20170720015643_create_authors.rb
@@ -1,8 +1,9 @@
+# frozen_string_literal: true
+
 class CreateAuthors < ActiveRecord::Migration[5.0]
   def change
     create_table :authors do |t|
-      t.string :name
-
+      t.string :name, null: false
       t.timestamps
     end
   end
