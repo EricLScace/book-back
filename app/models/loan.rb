598c0312bb404c4d22f5731694c986829e3ac36b
EricLScace <mail@ericscace.com>
2017-07-20 00:05:44 -0400

    Add inverse_of relationships to loans
    Why: clarifies 2-way relationship in Rails so that
    changes propagate
    
    Issue #20

--- app/models/loan.rb
+++ app/models/loan.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
 class Loan < ApplicationRecord
-  belongs_to :user
-  belongs_to :book
+  belongs_to :user, inverse_of: :loans
+  belongs_to :book, inverse_of: :loans
 end
