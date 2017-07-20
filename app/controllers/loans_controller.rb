55c5c7647c5a24b3d8d689441afac3e3172ed457
EricLScace <mail@ericscace.com>
2017-07-20 00:19:25 -0400

    De-lint loans controller
    Issue #20

--- app/controllers/loans_controller.rb
+++ app/controllers/loans_controller.rb
@@ -1,5 +1,7 @@
+# frozen_string_literal: true
+
 class LoansController < ApplicationController
-  before_action :set_loan, only: [:show, :update, :destroy]
+  before_action :set_loan, only: %i[show update destroy]
 
   # GET /loans
   def index
@@ -39,13 +41,14 @@ class LoansController < ApplicationController
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_loan
-      @loan = Loan.find(params[:id])
-    end
 
-    # Only allow a trusted parameter "white list" through.
-    def loan_params
-      params.require(:loan).permit(:user_id, :book, :date)
-    end
+  # Use callbacks to share common setup or constraints between actions.
+  def set_loan
+    @loan = Loan.find(params[:id])
+  end
+
+  # Only allow a trusted parameter "white list" through.
+  def loan_params
+    params.require(:loan).permit(:user_id, :book, :date)
+  end
 end
