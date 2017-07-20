3202ce963bd3701dc6240527a57e95b223449260
EricLScace <mail@ericscace.com>
2017-07-19 23:51:16 -0400

    Fix migration for loans join
    What:
    * correct misspelled 'references' caused some missing info
    * require timestamp of loan
    
    Issue #20

--- db/migrate/20170720034545_create_loans.rb
+++ db/migrate/20170720034545_create_loans.rb
@@ -1,11 +1,13 @@
+# frozen_string_literal: true
+
 class CreateLoans < ActiveRecord::Migration[5.0]
   def change
     create_table :loans do |t|
       t.references :user, foreign_key: true
-      t.refereences :book
+      t.references :book, foreign_key: true
       t.datetime :date
 
-      t.timestamps
+      t.timestamps null: false
     end
   end
 end
