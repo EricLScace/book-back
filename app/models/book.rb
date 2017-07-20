026395d93348e5e48040e4f4426a4841e0f2f80e
EricLScace <mail@ericscace.com>
2017-07-20 00:37:41 -0400

    Add thru & dependent:destroys to book user owner
    Further model tweaks for join tables
    
    Issue#22

--- app/models/book.rb
+++ app/models/book.rb
@@ -3,7 +3,9 @@
 class Book < ApplicationRecord
   belongs_to :author
   has_many :users, through: :loans
+  has_many :users, through: :owners
   has_many :loans, dependent: :destroy
+  has_many :owners, dependent: :destroy
   validates :title, presence: true
   validates :author_id, presence: true
 end
